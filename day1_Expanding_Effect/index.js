f8ed56966256c4efa861dd0285eea9b9a9a76f77
Lavanya-Sathya <sathyalavanya5@gmail.com>
2024-01-31 11:48:00 +0530

    project-file-updated

--- day1_Expanding_Effect/index.js
+++ day1_Expanding_Effect/index.js
@@ -2,14 +2,18 @@
 let controlWidth = document.querySelectorAll(".imgEffect");
 
 controlWidth.forEach((img)=>{
+    const imgCaption = img.querySelector(".caption");
     img.addEventListener("click",()=>{
         removeExpandClass()
         img.classList.add("imgExpand");
+        imgCaption.classList.add("captionDisplay");
     })
 })
 
 function removeExpandClass(){
     controlWidth.forEach((img) => { 
+        const imgCaption = img.querySelector(".caption");
         img.classList.remove("imgExpand");
+        imgCaption.classList.remove("captionDisplay");
     });
 }
--- script.js
+++ script.js
@@ -10,20 +10,3 @@ cardHeader.forEach((card)=>{
       card.classList.remove("cardHeaderOpacity");
     });
 })
-
-
-// const cardHeaders = document.querySelectorAll(".cardHeader");
-
-// cardHeaders.forEach((card) => {
-//   const cardHover = card.querySelector(".cardHover");
-
-//   card.addEventListener("mouseover", () => {
-//     cardHover.classList.add("cardHoverOpacity");
-//     card.classList.add("cardHeaderOpacity");
-//   });
-
-//   card.addEventListener("mouseout", () => {
-//     cardHover.classList.remove("cardHoverOpacity");
-//     card.classList.remove("cardHeaderOpacity");
-//   });
-// });
